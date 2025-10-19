de0c9f3c435add2b085849f18897eef59fd55d97
Cristian Contreras <cristiancontreras.it@gmail.com>
2025-10-19 20:05:12 -0300

    agrega for

---
+++ 02_controles_de_flujo/02_for.py
@@ -0,0 +1,16 @@
+# -------------------------
+# Forma 1: for con in
+# -------------------------
+def mostrar_lista():
+    lista=[1,2,3]
+    for item in lista:
+        print(item)
+# -------------------------
+# Forma 2: if en una línea
+# -------------------------
+def main():
+    mostrar_lista()
+
+if __name__=="__main__":
+    main()
+
